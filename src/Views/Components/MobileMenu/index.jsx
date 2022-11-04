673e4108dd3afe3493f01a419f629e4c7717d4e5
sigltech <101203107+sigltech@users.noreply.github.com>
2022-11-04 14:56:35 +0000

    changes mobileMenu buttons from buttons to react-router-dom Links

--- src/Views/Components/MobileMenu/index.jsx
+++ src/Views/Components/MobileMenu/index.jsx
@@ -3,38 +3,43 @@ import { MdOutlineSpaceDashboard } from 'react-icons/md';
 import { AiOutlineBug } from 'react-icons/ai';
 import { IoCreateOutline } from 'react-icons/io5';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-export default function MobileMenu(props) {
-    const navigate = useNavigate();
+export default function MobileMenu() {
     const { user } = useSelector(state => state);
     return (
         <div className=' z-50 md:translate-y-[100%] fixed bottom-0 w-full bg-slate-600 h-24'>
             <div className='w-full grid grid-flow-col h-full'>
-                <div onClick={() => navigate('/')} className='border-r-2 flex justify-center items-center h-full w-full'>
-                    <button className='flex items-center justify-center h-full w-full'>
+                <div
+                    className='border-r-2 flex justify-center items-center h-full w-full'
+                >
+                    <Link to='/' className='flex items-center justify-center h-full w-full'>
                         <span className='nav-link-icon text-2xl mr-3'>
                             <MdOutlineSpaceDashboard />
                         </span>
                         <span>Dashboard</span>
-                    </button>
+                    </Link>
                 </div>
-                <div onClick={() => navigate('/viewbugs')} className='border-r-2 flex justify-center items-center h-full w-full'>
-                    <button className='flex items-center justify-center h-full w-full'>
+                <div
+                    className='border-r-2 flex justify-center items-center h-full w-full'
+                >
+                    <Link to='/viewbugs' className='flex items-center justify-center h-full w-full'>
                         <span className='nav-link-icon text-2xl mr-3'>
                             <AiOutlineBug />
                         </span>
                         <span>View Bugs</span>
-                    </button>
+                    </Link>
                 </div>
                 {user.admin &&
-                    <div onClick={() => navigate('create')} className='border-r-2 flex justify-center items-center h-full w-full'>
-                        <button className='flex items-center justify-center h-full w-full'>
+                    <div
+                        className='border-r-2 flex justify-center items-center h-full w-full'
+                    >
+                        <Link to='/create' className='flex items-center justify-center h-full w-full'>
                             <span className='nav-link-icon text-2xl mr-3'>
                                 <IoCreateOutline />
                             </span>
                             <span>Create Bug</span>
-                        </button>
+                        </Link>
                     </div>
                 }
             </div>
