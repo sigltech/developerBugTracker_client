4661de9e717b0aaa072425314af31493f54b9e8d
sigltech <101203107+sigltech@users.noreply.github.com>
2022-11-04 13:07:41 +0000

    adds conditional to bugviewsection component to change color if the props.info is "completed"

--- src/Views/Components/BugView/Component/BugViewSection.jsx
+++ src/Views/Components/BugView/Component/BugViewSection.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import './BugViewSection.css'
 
 export default function BugViewSection(props) {
+
+
+
     return (
-        <div className='view-section'>
-            <h2>{props.title}</h2>
+        <div className={props.info === 'completed' ? ' text-white m-[10px] p-[10px] rounded-[5px] text-center min-w-[40%] bg-green-600' : 'view-section'}>
+            <h2 className={
+                props.info === 'completed' ? 'mb-[15px] border-b-2 border-white' : 'mb-[15px] border-b-2 border-black'
+            }>
+                {props.title}
+            </h2>
             <p>{props.info}</p>
         </div>
     )
